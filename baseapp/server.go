71515ca9d15ff754b143e2f3e1aa6d2f397a2d6c
Javier Campanini <jcampanini@palantir.com>
2021-09-13 12:30:12 -0400

    add cipher list from mozilla recommended

--- baseapp/server.go
+++ baseapp/server.go
@@ -80,6 +80,22 @@ func NewServer(c HTTPConfig, params ...Param) (*Server, error) {
 		base.server = &http.Server{
 			TLSConfig: &tls.Config{
 				MinVersion: tls.VersionTLS12,
+				CipherSuites: []uint16{
+					// http/2 support
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+
+					// intermediate/recommended compatibility for server-side TLS
+					// https://wiki.mozilla.org/Security/Server_Side_TLS#Intermediate_compatibility_.28recommended.29
+					tls.TLS_AES_128_GCM_SHA256,
+					tls.TLS_AES_256_GCM_SHA384,
+					tls.TLS_CHACHA20_POLY1305_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+				},
 			},
 		}
 	}
